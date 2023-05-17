9966395e8adf251f06976a707ee76b650fff671c
Ganesh Enumula <ganesh.enumula@capgemini.com>
2023-05-17 14:23:15 +0530

    Commiting cs file

---
+++ ConsoleApp1/ConsoleApp1/Addition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Addition
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(25+54);
+        }
+    }
+}
